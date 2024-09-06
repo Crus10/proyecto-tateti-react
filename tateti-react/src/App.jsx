aa904ad842748002f96e75a4d96e9ab46a7f07fe
Crus10 <juan.cruslol@gmail.com>
2024-09-06 14:10:26 -0300

    Proyecto tateti
    Aprendiendo a implementar Reack , useState y hooks

---
+++ tateti-react/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+
+const TURNS = {
+  X: "☠",
+  O: "🥵",
+}
+
+
+
+const Square = ({ children, updateBoard, index })=> {
+  return (
+    <div className="square">
+      {children}
+    </div>
+  )
+}
+
+function App() {
+  const [board, setBoard] = useState(
+  Array(9).fill(null)
+)
+  const [turn, setTurn] = useState(TURNS.X)
+
+  
+  return (
+    <main className="board">
+
+    <h1> TA TE TI </h1>
+    <section className="game">
+    {
+      board.map((_, index)=> {
+        return (
+          <Square
+            key={index}
+            index={index}
+            >
+              {index}
+             </Square>
+            
+        )
+      })
+    }
+    </section>
+    </main>
+  )
+
+}
+export default App
